1062ef22940a3df4ab48eb3234368b11dc8dc191
Andrew <alex.andrew@sanmina.com>
2017-08-26 23:49:07 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NoAccessComponent } from './no-access/no-access.component';
+const routes = [
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'admin', component: AdminComponent},
+      {path: 'no-access', component: NoAccessComponent},
+      {path: 'login', component: LoginComponent}
+    ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent,
+    NoAccessComponent
   ],
   imports: [
-    BrowserModule
+     BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
